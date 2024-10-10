db9cd50be0b47823a01d5c24f9485fb71e16b4af
danichagas <chagasdaniel788@gmail.com>
2024-10-10 17:37:32 -0300

    Função logRoll

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const palyer1 = {
+const player1 = {
   name: 'Mario',
   speed: 4,
   maneuverability: 3,
@@ -39,6 +39,10 @@ async function getRandomBlock() {
   return result
 }
 
+async function logRollResult(characterName, block, diceResult, atribute) {
+  console.log(`${characterName} rolou um dado de ${block} ${diceResult}`)
+}
+
 async function raceEngine(character1, character2) {
   for (let round = 1; round <= 5; round++) {
     console.log(`🏁 Rodada ${round}`)
@@ -46,6 +50,34 @@ async function raceEngine(character1, character2) {
     // Sorteio do bloco
     let block = await getRandomBlock()
     console.log(`Bloco: ${block}`)
+
+    // Rolar os dados
+    let diceResult1 = await rollDice()
+    let diceResult2 = await rollDice()
+
+    // Teste de habilidade
+    let testSkill1 = 0
+    let testSkill12 = 0
+
+    if (block === "RETA") {
+      testSkill1 = diceResult1 + character1.speed
+      testSkill12 = diceResult2 + character2.speed
+
+      await logRollResult(player1.name, 'velocidade', diceResult1, character1.speed)
+      await logRollResult(player2.name, 'velocidade', diceResult2, character2.speed)
+    }
+
+    if (block === "CURVA") {
+      testSkill1 = diceResult1 + character1.maneuverability
+      testSkill12 = diceResult2 + character2.maneuverability
+
+      await logRollResult(player1.name, 'manobrabilidade', diceResult1, character1.maneuverability)
+      await logRollResult(player2.name, 'manobrabilidade', diceResult2, character2.maneuverability)    }
+
+    if (block === "CONFRONTO") {
+      let powerResult1 = diceResult1 + character1.power
+      let powerResult2 = diceResult2 + character2.power
+    }
   }
 }
 
