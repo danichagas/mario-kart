168e05cccf46f5fe922dc956a44b192710df8749
danichagas <chagasdaniel788@gmail.com>
2024-10-10 14:16:08 -0300

    Blocks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const palyer = {
+const palyer1 = {
   name: 'Mario',
   speed: 4,
   maneuverability: 3,
@@ -18,6 +18,39 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1 
 }
 
+async function getRandomBlock() {
+  let random = Math.random()
+  let result
+
+  switch (true) {
+    case random < 0.33:
+      result = "RETA"
+      break
+
+    case random < 0.66:
+      result = "CURVA"
+      break
+
+      default:
+        result = "CONFRONTO"
+      break
+  }
+
+  return result
+}
+
+async function raceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Rodada ${round}`)
+
+    // Sorteio do bloco
+    let block = await getRandomBlock()
+    console.log(`Bloco: ${block}`)
+  }
+}
+
 (async function main() {
-  console.log('teste')
+  console.log(`🏁🚨 Corrida entre ${palyer1.name} e ${player2.name} começando...\n`)
+
+  await raceEngine(palyer1, player2)
 })()
